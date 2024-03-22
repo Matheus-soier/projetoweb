1cb7e5a9a275fa8ae2c5d76c67c60827a909b95f
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2024-03-22 11:13:49 -0300

    updates

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ const app = express();
 const handlebars = require("express-handlebars").engine;
 
 app.engine("handlebars", handlebars({defaultLayout: "main"}));
-
 app.set("view engine", "handlebars");
+
+const bodyParser = require("body-parser");
+const post = require("./models/post");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+app.post("/cadastrar", function(req, res){
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function(){
+        res.redirect("/");
+    }).catch(function(erro){
+        res.send("Falha ao cadastrar os dados: " + erro);
+    })
 });
 
 app.get("/consultar", (req, res) => {
